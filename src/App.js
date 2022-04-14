ac0cf13dc4f93cec8fa45d5a8197508df5c496a7
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2022-04-14 17:02:32 +0300

    adding-basic-css-styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <h2>Let's get started!!111</h2>
+      <div><ExpenseItem/></div>
     </div>
   );
 }
