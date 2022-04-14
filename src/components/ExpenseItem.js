791223df45c83f0632e7a1ea3c348c9604852a0f
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2022-04-14 17:09:18 +0300

    outputting-dynamic-data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2021, 2, 28)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.69
+
     return (
         <div className="expense-item">
-            <div>April 14th 2021</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$293.69</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     )
